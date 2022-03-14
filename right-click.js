ad2d91d12a9b3037ecff154c076388f69317c839
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-03-14 09:28:40 -0700

    Update right-click.js

--- right-click.js
+++ right-click.js
@@ -1,7 +1,21 @@
 if (document.addEventListener) {
   document.addEventListener('contextmenu', function(e) {
-    alert("You've tried to open context menu"); //here you draw your own menu
+    
+    function loadgame(https://bing.com) {
+      var url = loc
+      var win;
+      win = window.open();
+      win.document.body.style.margin = "0";
+      win.document.body.style.height = "100vh";
+      var iframe = win.document.createElement("iframe");
+      iframe.style.border = "none";
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.margin = "0";
+      iframe.src = url;
+      win.document.body.appendChild(iframe);
     e.preventDefault();
+      
   }, false);
 } else {
   document.attachEvent('oncontextmenu', function() {
