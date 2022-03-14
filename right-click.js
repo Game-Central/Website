40c312477bfc299003064af8d198cf10ac537098
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-03-14 09:32:04 -0700

    Update right-click.js

--- right-click.js
+++ right-click.js
@@ -1,25 +1,38 @@
 if (document.addEventListener) {
   document.addEventListener('contextmenu', function(e) {
     
-    function loadgame(https://bing.com) {
-      var url = loc
-      var win;
-      win = window.open();
-      win.document.body.style.margin = "0";
-      win.document.body.style.height = "100vh";
-      var iframe = win.document.createElement("iframe");
-      iframe.style.border = "none";
-      iframe.style.width = "100%";
-      iframe.style.height = "100%";
-      iframe.style.margin = "0";
-      iframe.src = url;
-      win.document.body.appendChild(iframe);
+    var url = "https://bing.com"
+    var win
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+    
     e.preventDefault();
       
   }, false);
 } else {
   document.attachEvent('oncontextmenu', function() {
-    alert("You've tried to open context menu");
-    window.event.returnValue = false;
+    
+    var url = "https://bing.com"
+    var win
+    win = window.open();
+    win.document.body.style.margin = "0";
+    win.document.body.style.height = "100vh";
+    var iframe = win.document.createElement("iframe");
+    iframe.style.border = "none";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.margin = "0";
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+    
+    e.preventDefault();
   });
 }
